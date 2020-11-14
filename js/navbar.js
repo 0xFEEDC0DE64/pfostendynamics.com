f5f27fcaf3f9e2b51e46606f35e3aa55865a9947
CommanderRedYT <ccommanderred@gmail.com>
2020-11-14 16:50:22 +0100

    Moved console log

--- js/navbar.js
+++ js/navbar.js
@@ -2,13 +2,12 @@ function setactivePage() {
   var path = window.location.pathname;
   var tmp = path.split("/").pop();
   var page = tmp.replace(".html", "");
-  console.log("page:" + page);
-
   var site = document.getElementsByClassName("canactive");
   len = site !== null ? site.length : 0;
   i = 0;
   for (i; i < len; i++) {
     if(page == "") page = "home";
+    console.log("page:" + page);
     var siteClass = document.getElementsByClassName(page);
     len2 = siteClass !== null ? siteClass.length : 0;
     j = 0;
