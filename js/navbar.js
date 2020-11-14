1ac3e42c64bd66fd937b04847468d170d0e6ca59
CommanderRedYT <ccommanderred@gmail.com>
2020-11-14 16:51:36 +0100

    Moved error correction

--- js/navbar.js
+++ js/navbar.js
@@ -2,17 +2,17 @@ function setactivePage() {
   var path = window.location.pathname;
   var tmp = path.split("/").pop();
   var page = tmp.replace(".html", "");
+  if (page == "") page = "home";
+  console.log("page:" + page);
   var site = document.getElementsByClassName("canactive");
   len = site !== null ? site.length : 0;
   i = 0;
   for (i; i < len; i++) {
-    if(page == "") page = "home";
-    console.log("page:" + page);
     var siteClass = document.getElementsByClassName(page);
     len2 = siteClass !== null ? siteClass.length : 0;
     j = 0;
-    for(j; j < len2; j++) {
-    siteClass[j].classList.add("active");
+    for (j; j < len2; j++) {
+      siteClass[j].classList.add("active");
     }
   }
 }
