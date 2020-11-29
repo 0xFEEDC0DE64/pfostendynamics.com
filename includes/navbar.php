1ae44a6a6cfb8f9215eb8404888faaf2450bd35c
CommanderRedYT <ccommanderred@gmail.com>
2020-11-29 03:59:33 +0100

    Formatted; Added non-drag-/selectable

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,7 @@
     <!--Begin Navbar-->
-    <nav
-      class="navbar navbar-expand-lg navbar-light bg-light couldDark"
-      id="navbar"
-    >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light couldDark disable-select" id="navbar">
       <div class="container">
-        <a class="navbar-brand" href="./">
+        <a class="navbar-brand" href="./" draggable="false" ondragstart="return false;">
           <img src="./img/PfostenDynamics.png" style="max-height: 60px" class="d-inline-block align-top" alt="" loading="lazy" id="pfostendynamics" />
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,25 +10,26 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <?php
-            function makeMenuItem($text, $url, $active) {
+            function makeMenuItem($text, $url, $active)
+            {
             ?>
-            <li class="nav-item home canactive <?php if ($active) { ?> active<?php } ?>">
-              <a class="nav-link" href="<?php echo htmlentities($url); ?>"><?php echo htmlentities($text); ?><?php if ($active) { ?> <span class="sr-only">(current)</span><?php } ?></a>
-            </li>
+              <li class="nav-item home canactive <?php if ($active) { ?> active<?php } ?>">
+                <a draggable="false" ondragstart="return false;" class="nav-link" href="<?php echo htmlentities($url); ?>"><?php echo htmlentities($text); ?><?php if ($active) { ?> <span class="sr-only">(current)</span><?php } ?></a>
+              </li>
             <?php
             }
             ?>
             <?php makeMenuItem("Home", "./", ACTIVE_MENUITEM == "home"); ?>
             <li class="nav-item dropdown vehicles bobbycar bobbyquad bobbybob bobbyklo bobbyboot bobbyhubschrauber raupenfahrzeug canactive">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Vehicles</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="./bobbycar">Bobbycar</a>
-                <a class="dropdown-item" href="./bobbyquad">Bobbyquad</a>
-                <a class="dropdown-item" href="./bobbybob">Bobbybob</a>
-                <a class="dropdown-item" href="./bobbyklo">Bobbyklo</a>
-                <a class="dropdown-item" href="./bobbyboot">Bobbyboot</a>
-                <a class="dropdown-item" href="./bobbyhubschrauber">Bobbyhubschrauber</a>
-                <a class="dropdown-item" href="./raupenfahrzeug">Raupenfahrzeug</a>
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" draggable="false" ondragstart="return false;">Vehicles</a>
+              <div class=" dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="./bobbycar" draggable="false" ondragstart="return false;">Bobbycar</a>
+                <a class="dropdown-item" href="./bobbyquad" draggable="false" ondragstart="return false;">Bobbyquad</a>
+                <a class="dropdown-item" href="./bobbybob" draggable="false" ondragstart="return false;">Bobbybob</a>
+                <a class="dropdown-item" href="./bobbyklo" draggable="false" ondragstart="return false;">Bobbyklo</a>
+                <a class="dropdown-item" href="./bobbyboot" draggable="false" ondragstart="return false;">Bobbyboot</a>
+                <a class="dropdown-item" href="./bobbyhubschrauber" draggable="false" ondragstart="return false;">Bobbyhubschrauber</a>
+                <a class="dropdown-item" href="./raupenfahrzeug" draggable="false" ondragstart="return false;">Raupenfahrzeug</a>
               </div>
             </li>
             <?php makeMenuItem("Careers", "./careers", ACTIVE_MENUITEM == "careers"); ?>
@@ -40,15 +38,7 @@
             <?php makeMenuItem("Shop", "./shop", ACTIVE_MENUITEM == "shop"); ?>
             <?php makeMenuItem("Impressum", "./impressum", ACTIVE_MENUITEM == "impressum"); ?>
           </ul>
-          <a class="nav-link" href="#" onclick="darkMode()"
-            ><img
-              id="darkModeImg"
-              class="img-fluid"
-              data-toggle="tooltip"
-              data-placement="top"
-              title="Toggle darkmode"
-              src=""
-          /></a>
+          <a class="nav-link" href="#" onclick="darkMode()" draggable="false" ondragstart="return false;"><img id="darkModeImg" class="img-fluid" data-toggle="tooltip" data-placement="top" title="Toggle darkmode" draggable="false" ondragstart="return false;" /></a>
         </div>
       </div>
     </nav>
