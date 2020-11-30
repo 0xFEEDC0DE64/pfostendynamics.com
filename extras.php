c2d6af3923872b3a4937636dac288109a60be90f
CommanderRedYT <ccommanderred@gmail.com>
2020-11-30 18:03:30 +0100

    Changed to extras-dir to avoid .htacces confusion

--- extras.php
+++ extras.php
@@ -17,8 +17,8 @@ require('includes/header.php');
     <div id="bobbycar-stl">
         <h3>Bobbycar</h3>
         <p>This is an .stl file containing an 3d model of a standard bobbycar (Click picture to download!)</p>
-        <a href="https://pfostendynamics.com/extras/bobbycar.stl" draggable="false" ondragstart="return false;">
-            <img class="img-fluid" src="https://pfostendynamics.com/extras/bobbycar.jpg" style="
+        <a href="./extras-dir/bobbycar.stl" draggable="false" ondragstart="return false;">
+            <img class="img-fluid" src="./extras-dir/bobbycar.jpg" style="
         max-width: 512px; 
         width: 100%; 
         border: 2px;
@@ -31,8 +31,8 @@ require('includes/header.php');
     <div id="logo-stl">
         <h3>Our Logo</h3>
         <p>Here we put our logo as an "plate", so you can print it out and put it somewhere so it can get dusty (Click picture to download!)</p>
-        <a href="https://pfostendynamics.com/extras/pfostendynamics.stl" draggable="false" ondragstart="return false;">
-            <img class="img-fluid" src="https://pfostendynamics.com/extras/pfostendynamics.jpg" style="
+        <a href="./extras-dir/pfostendynamics.stl" draggable="false" ondragstart="return false;">
+            <img class="img-fluid" src="./extras-dir/pfostendynamics.jpg" style="
         max-width: 512px; 
         width: 100%; 
         border: 2px;
@@ -44,9 +44,9 @@ require('includes/header.php');
     <hr class="couldDark">
     <div id="kartoffelfahrzeug">
         <h3>Der Kartoffelfahrzeug (The potato-vehicle)</h3>
-        <p>This was <a href="https://pfostendynamics.com/about#greyhash">greyhashs</a> attempt of creating his own "instance" of a bobbycar. It was powered by two
+        <p>This was <a href="./about#greyhash">greyhashs</a> attempt of creating his own "instance" of a bobbycar. It was powered by two
             motors, a <a href="https://en.wikipedia.org/wiki/Club-Mate" target="_blank" rel="noopener"> mate box </a> and a not so good steering wheel in the form of two sliding Potentiometers. And after an unscheduled disassembly there is now a better built bobby car, whereby (as of 2020) it is still broken ...</p>
-        <img class="img-fluid" src="https://pfostendynamics.com/extras/kartoffel.jpg" style="
+        <img class="img-fluid" src="./extras-dir/kartoffel.jpg" style="
         max-width: 512px; 
         width: 100%; 
         border: 2px;
