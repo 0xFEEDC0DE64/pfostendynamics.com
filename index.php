3049ab7ddfb0b3daab39c93d87a90833e667c0e9
CommanderRedYT <ccommanderred@gmail.com>
2020-12-01 04:55:42 +0100

    Little fix for background on highlight

--- index.php
+++ index.php
@@ -5,137 +5,99 @@ define('ACTIVE_MENUITEM', 'home');
 
 require('includes/header.php');
 ?>
-    <br />
-    <br />
-    <br />
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm text-right">
-          <br />
-          <h1 class="text-shadow couldDark <?php if($_COOKIE["darkmode"] == "true") echo "darkMode"; ?>" style="text-shadow: 1.3px 1.3px #686868">Pfosten Dynamics</h1>
-          <h2>Change your idea of what idiots can do</h2>
-        </div>
-        <div class="col-sm text-left">
-          <br />
-          <img
-            src="./img/peterXfeedc0de.jpg"
-            style="
+<br />
+<br />
+<br />
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm text-right">
+      <br />
+      <h1 class="text-shadow couldDark <?php if ($_COOKIE["darkmode"] == "true") echo "darkMode"; ?>" style="text-shadow: 1.3px 1.3px #686868">Pfosten Dynamics</h1>
+      <h2>Change your idea of what idiots can do</h2>
+    </div>
+    <div class="col-sm text-left">
+      <br />
+      <img src="./img/peterXfeedc0de.jpg" style="
               max-height: 600px;
               border: 2px;
               border-style: ridge;
               border-color: gray;
-            "
-            class="d-inline-block shadow rounded img-fluid"
-            alt=""
-            loading="lazy"
-            draggable="false"
-            ondragstart="return false;"
-          />
-        </div>
-      </div>
-      <br />
-      <br />
-      <div
-        class="container-fluid text-center"
-        style="background-color: rgba(165, 165, 165, 0.372)"
-      >
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm text-right vert">
-              <br />
-              <img
-                src="./img/bobbycars.png"
-                style="
+            " class="d-inline-block shadow rounded img-fluid" alt="" loading="lazy" draggable="false" ondragstart="return false;" />
+    </div>
+  </div>
+  <br />
+  <br />
+  <div class="container-fluid text-center highlight">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm text-right vert">
+          <br />
+          <img src="./img/bobbycars.png" style="
                   max-height: 600px;
                   border: 2px;
                   border-style: ridge;
                   border-color: gray;
-                "
-                class="d-inline-block shadow rounded img-fluid"
-                alt=""
-                loading="lazy"
-                draggable="false"
-                ondragstart="return false;"
-              />
-            </div>
-            <div class="col-sm text-left">
-              <br />
-              <h1 style="text-shadow: 1.3px 1.3px #686868">THE BOBBYCARS</h1>
-              <h2>
-                The mobile bobbycar designed for driving against walls, going up
-                in smoke, and hurting the driver.
-              </h2>
-            </div>
-          </div>
+                " class="d-inline-block shadow rounded img-fluid" alt="" loading="lazy" draggable="false" ondragstart="return false;" />
+        </div>
+        <div class="col-sm text-left">
           <br />
+          <h1 style="text-shadow: 1.3px 1.3px #686868">THE BOBBYCARS</h1>
+          <h2>
+            The mobile bobbycar designed for driving against walls, going up
+            in smoke, and hurting the driver.
+          </h2>
         </div>
       </div>
       <br />
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm text-right">
-            <br />
-            <h1 style="text-shadow: 1.3px 1.3px #686868">TRANSPORT</h1>
-            <h2>If the bobbycar works, it can be used for many applications</h2>
-          </div>
-          <div class="col-sm text-left">
-            <br />
-            <img
-              src="./img/feedcode-bobbycar-1.jpg"
-              style="
+    </div>
+  </div>
+  <br />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm text-right">
+        <br />
+        <h1 style="text-shadow: 1.3px 1.3px #686868">TRANSPORT</h1>
+        <h2>If the bobbycar works, it can be used for many applications</h2>
+      </div>
+      <div class="col-sm text-left">
+        <br />
+        <img src="./img/feedcode-bobbycar-1.jpg" style="
                 max-height: 600px;
                 border: 2px;
                 border-style: ridge;
                 border-color: gray;
-              "
-              class="d-inline-block shadow rounded img-fluid"
-              alt=""
-              loading="lazy"
-              draggable="false"
-              ondragstart="return false;"
-            />
-          </div>
-        </div>
-        <br />
-        <br />
-        <div
-          class="container-fluid text-center"
-          style="background-color: rgba(165, 165, 165, 0.372)"
-        >
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-sm text-right vert">
-                <br />
-                <img
-                  src="./img/party.jpg"
-                  style="
+              " class="d-inline-block shadow rounded img-fluid" alt="" loading="lazy" draggable="false" ondragstart="return false;" />
+      </div>
+    </div>
+    <br />
+    <br />
+  </div>
+  <div class="container-fluid text-center highlight">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm text-right vert">
+          <br />
+          <img src="./img/party.jpg" style="
                     max-height: 600px;
                     border: 2px;
                     border-style: ridge;
                     border-color: gray;
-                  "
-                  class="d-inline-block shadow rounded img-fluid"
-                  alt=""
-                  loading="lazy"
-                  draggable="false"
-                  ondragstart="return false;"
-                />
-              </div>
-              <div class="col-sm text-left">
-                <br />
-                <h1 style="text-shadow: 1.3px 1.3px #686868">
-                  PERSON TRANSPORT
-                </h1>
-                <h2>
-                  If you want to transport your friends, no problem, but be
-                  careful that the wheels of the trailer can take the weight.
-                </h2>
-              </div>
-            </div>
-            <br />
-          </div>
+                  " class="d-inline-block shadow rounded img-fluid" alt="" loading="lazy" draggable="false" ondragstart="return false;" />
         </div>
-        <br />
+        <div class="col-sm text-left">
+          <br />
+          <h1 style="text-shadow: 1.3px 1.3px #686868">
+            PERSON TRANSPORT
+          </h1>
+          <h2>
+            If you want to transport your friends, no problem, but be
+            careful that the wheels of the trailer can take the weight.
+          </h2>
+        </div>
       </div>
+      <br />
     </div>
+  </div>
+  <br />
+</div>
 <?php require('includes/footer.php'); ?>
